e34084a409a2708eab3d4d4942dec0746d88ec59
akash418 <418akash@gmail.com>
2024-11-29 14:55:29 +0100

    adding verification method

--- eval/main.py
+++ eval/main.py
@@ -7,16 +7,25 @@ from wikidata_utils import *
 def main(
         wikidata_triples_file_path:str,
         model_name:str,
-        seed: str,      
+        seed: str,
+        verification_method: str      
 ):
-
+    valid_methods = ["web", "wikidata"]
+    if verification_method not in valid_methods:
+        raise ValueError(f"Invalid verification method. Choose from {valid_methods}")
+    
     process_request = ProcessRequest(model_name, wikidata_triples_file_path, seed)
     ret_triples = process_request.read_triples_file()
-    #ret_triples_with_snippet = process_request.query_snippets(ret_triples)
-    #output_dict = process_request.verify_triples(ret_triples_with_snippet)
-    #print("output_dict", output_dict)
 
-    plausible_claims = soft_match_utils(ret_triples)
+    if verification_method == "web":
+        ret_triples_with_snippet = process_request.query_snippets(ret_triples)
+        output_dict = process_request.verify_triples(ret_triples_with_snippet)
+        print("Output dictionary recording triple verification ... ", output_dict)
+    
+    if verification_method == "wikidata":
+        plausible_claims = soft_match_utils(ret_triples)
+        print(f"Fraction of triples plausible: {len(plausible_claims)/len(ret_triples)}")
+
 
 
 
